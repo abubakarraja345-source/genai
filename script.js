a12ced6487add464e8df32846e6647e18b25c49e
abubakarraja345-source <abubakarraja345@gmail.com>
2025-08-15 10:35:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://genai-sandy.vercel.app/chat';
+const API_URL = 'http://127.0.0.1:5000/chat';
 
 function displayPersonality() {
   const personality = localStorage.getItem('chatPersonality') || "Friendly";
